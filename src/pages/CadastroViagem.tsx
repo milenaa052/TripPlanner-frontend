21df0d5271595b3193453fc0b2f6261bfa8c4f94
milenaa_052 <milenasantosdeoliveira40@gmail.com>
2025-03-15 19:02:38 -0300

    Integrando api do backend

--- src/pages/CadastroViagem.tsx
+++ src/pages/CadastroViagem.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
@@ -5,6 +6,32 @@ import "react-datepicker/dist/react-datepicker.css";
 function CadastroViagem() {;
     const [dataInicio, setDataInicio] = useState<Date | null>(null);
     const [dataFim, setDataFim] = useState<Date | null>(null);
+    const [mensagem, setMensagem] = useState("");
+    const [localOrigem, setLocalOrigem] = useState("");
+    const [localDestino, setLocalDestino] = useState("");
+    const [dataInicial, setDataInicial] = useState("");
+    const [dataFinal, setDataFinal] = useState("");
+
+    const enviarForm = async () => {
+        try {
+            await axios.post(`http:localhost:3000/cadastro-viagem`, {
+                localOrigem,
+                localDestino,
+                dataInicial,
+                dataFinal
+            })
+
+            setMensagem("Cadastro realizado com sucesso.");
+
+            setLocalOrigem("");
+            setLocalDestino("");
+            setDataInicial("");
+            setDataFinal("");
+        } catch (error) {
+            setMensagem("Erro ao salvar dados");
+            console.log(error)
+        }
+    }
 
     const manipularDatas = (range: [Date | null, Date | null]) => {
         const [startDate, endDate] = range;
@@ -19,12 +46,14 @@ function CadastroViagem() {;
             <form className="form">
                 <div className="campos">
                     <label htmlFor="localOrigem" className="label">Local de Origem</label>
-                    <input type="text" id="localOrigem" name="localOrigem" className="input" placeholder="Insira a cidade de origem"/>
+                    <input type="text" id="localOrigem" name="localOrigem" className="input" 
+                        value={localOrigem} onChange={(e) => setLocalDestino(e.target.value)} placeholder="Insira a cidade de origem"/>
                 </div>
 
                 <div className="campos">
                     <label htmlFor="localDestino" className="label">Destino</label>
-                    <input type="text" id="localDestino" name="localDestino" className="input" placeholder="Insira a cidade de destino"/>
+                    <input type="text" id="localDestino" name="localDestino" className="input" 
+                       value={localDestino} onChange={(e) => setLocalDestino(e.target.value)} placeholder="Insira a cidade de destino"/>
                 </div>
                 
                 <div className="campos">
@@ -34,9 +63,11 @@ function CadastroViagem() {;
                 </div>
 
                 <div className="submit">
-                    <button type="submit" className="salvar">Salvar</button>
+                    <button type="submit" className="salvar" onSubmit={enviarForm}>Salvar</button>
                 </div>
             </form>
+
+            { mensagem && <p>{mensagem}</p> }
       </div>
     );
 };
