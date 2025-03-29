802ff247625ce571e98bb3ffb08cfa0cb5344e55
milenaa_052 <milenasantosdeoliveira40@gmail.com>
2025-03-29 18:09:23 -0300

    Update em despesas

--- src/components/ModalDespesa.tsx
+++ src/components/ModalDespesa.tsx
@@ -3,6 +3,8 @@ import "react-datepicker/dist/react-datepicker.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import ConfirmaExclusao from "./ConfirmaExclusao";
+import axios from "axios";
+import { useParams } from "react-router";
 
 interface DespesaProps {
     idDespesa: number;
@@ -22,10 +24,37 @@ function ModalDespesa({ despesa, onDelete, onClose }: ModalDespesaProps) {;
     const [tipoDespesa, setTipoDespesa] = useState(despesa.tipoDespesa);
     const [gasto, setGasto] = useState(despesa.gasto.toString());
     const [dataDespesa, setDataDespesa] = useState(despesa.dataDespesa);
+    const [mensagem, setMensagem] = useState("");
 
+    const { id } = useParams();
+
+    const atualizarDespesa = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        try {
+            await axios.put(`http://localhost:3000/despesa/${despesa.idDespesa}`, {
+                tipoDespesa,
+                gasto: parseFloat(gasto),
+                dataDespesa,
+                viagemId: Number(id)
+            })
+
+            setMensagem("Despesa atualizada com sucesso!");
+            onClose();
+
+        } catch (error) {
+            setMensagem("Erro ao atualizar a despesa");
+            console.error(error);
+        }
+    }
+
+    const formatarData = (data: string) => {
+        return data.slice(0, 10);
+    };
+    
     return (
         <div className="card">
-            <form className="form">
+            <form className="form" onSubmit={atualizarDespesa}>
                 <div className="campos">
                     <label htmlFor="tipoDespesa" className="label">Tipo de Despesa</label>
                     <input type="text" id="tipoDespesa" name="tipoDespesa" className="input" 
@@ -41,7 +70,7 @@ function ModalDespesa({ despesa, onDelete, onClose }: ModalDespesaProps) {;
                 <div className="campos">
                     <label htmlFor="data" className="label">Data</label>
                     <input type="date" id="data" name="data" className="input" 
-                       value={dataDespesa} onChange={(e) => setDataDespesa(e.target.value)} placeholder="Insira a data da despesa"/>
+                       value={formatarData(dataDespesa)} onChange={(e) => setDataDespesa(e.target.value)} placeholder="Insira a data da despesa"/>
                 </div>
 
                 <div className="submit">
@@ -51,6 +80,8 @@ function ModalDespesa({ despesa, onDelete, onClose }: ModalDespesaProps) {;
                     <button type="submit" className="salvar">Salvar</button>
                 </div>
             </form>
+
+            { mensagem ? <p>{ mensagem }</p> : "" }
 
             {mostrarConfirmacao && (
                 <ConfirmaExclusao 
