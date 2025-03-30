bffd53fddadb6dfc97e823a0924dc24aaefcf65f
milenaa_052 <milenasantosdeoliveira40@gmail.com>
2025-03-30 15:53:14 -0300

    Criando rotas autenticadas e validando login

---
+++ src/components/PrivateRoutes.tsx
@@ -0,0 +1,15 @@
+import { Navigate } from "react-router";
+import { useAuth } from "../contexts/AuthContext";
+import { JSX } from "react";
+
+interface Props {
+    children: JSX.Element;
+}
+
+const PrivateRoute = ({ children }: Props) => {
+    const { estaAutenticado } = useAuth();
+
+    return estaAutenticado ? children : <Navigate to="/login" />
+}
+
+export default PrivateRoute;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,17 +1,53 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { useAuth } from "../contexts/AuthContext";
+
 function Login () {
+    const [mensagem, setMensagem] = useState("")
+    const [email, setEmail] = useState("")
+    const [senha, setSenha] = useState("")
+    const { login } = useAuth();
+    const navigate = useNavigate();
+
+    const enviarLogin = async (e: React.FormEvent) => {
+        e.preventDefault()
+
+        try {
+            const response = await axios.post("http://localhost:3000/login", {
+                email,
+                senha
+            })
+
+            const token = response.data.token
+            login(token)
+            navigate("/")
+
+            setMensagem("Login realizado com sucesso!")
+
+            setEmail("")
+            setSenha("")
+        } catch (error) {
+            setMensagem("Não foi possível realizar o login")
+            console.error(error)
+        }
+    }
+
     return (
         <div className="card">
             <h1 className="titulo">Login</h1>
 
-            <form className="form">
+            <form className="form" onSubmit={enviarLogin}>
                 <div className="campos">
                     <label htmlFor="email" className="label">Email</label>
-                    <input type="email" id="email" name="email" className="input" placeholder="Insira o seu email"/>
+                    <input type="email" id="email" name="email" className="input" autoComplete="off"
+                       value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Insira o seu email"/>
                 </div>
 
                 <div className="campos">
                     <label htmlFor="senha" className="label">Senha</label>
-                    <input type="password" id="senha" name="senha" className="input" placeholder="Insira a sua senha"/>
+                    <input type="password" id="senha" name="senha" className="input" autoComplete="off"
+                       value={senha} onChange={(e) => setSenha(e.target.value)} placeholder="Insira a sua senha"/>
                 </div>
 
                 <div className="submitAuth">
@@ -19,6 +55,8 @@ function Login () {
                     <button type="submit" className="entrar">Entrar</button>
                 </div>
             </form>
+
+            { mensagem ? <p>{ mensagem }</p> : "" }
       </div>
     );
 };
